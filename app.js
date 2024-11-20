522fa7ca956172b4789be951876e7c51a084dbff
enzosoft <enzo.leandrini@sou.unifeob.edu.br>
2024-11-20 15:45:53 -0300

    dev_frontend

--- app.js
+++ app.js
@@ -7,15 +7,27 @@ const PORT = 4040;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-// rota para api externa
+// rota para api externa 
+// app.get('/', async (req, res) => {
+//   try {
+//     const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json'); 
+//     const dados = response.data;
+//     res.render('index', { dados });
+//   } catch (error) {
+//     console.error(error);
+//     res.render('index', { dados: [] });
+//   }
+// });
+
 app.get('/', async (req, res) => {
+  const limit = parseInt(req.query.limit) || 10; // Limite inicial (10 produtos por padrão)
   try {
     const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json'); 
-    const dados = response.data;
-    res.render('index', { dados });
+    const dados = response.data.slice(0, limit); // Seleciona os primeiros 'limit' produtos
+    res.render('index', { dados, limit });
   } catch (error) {
     console.error(error);
-    res.render('index', { dados: [] });
+    res.render('index', { dados: [], limit });
   }
 });
 

106c75bb5ebb93a50eceb80dbf568eb888d0ff9b
Enzo Dorigon Leandrini <enzo.leandrini@sou.unifeob.edu.br>
2024-11-20 17:50:42 -0300

    app.js

--- app.js
+++ app.js
@@ -7,29 +7,29 @@ const PORT = 4040;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-// rota para api externa 
-// app.get('/', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json'); 
-//     const dados = response.data;
-//     res.render('index', { dados });
-//   } catch (error) {
-//     console.error(error);
-//     res.render('index', { dados: [] });
-//   }
-// });
-
+rota para api externa 
 app.get('/', async (req, res) => {
-  const limit = parseInt(req.query.limit) || 10; // Limite inicial (10 produtos por padrão)
   try {
     const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json'); 
-    const dados = response.data.slice(0, limit); // Seleciona os primeiros 'limit' produtos
-    res.render('index', { dados, limit });
+    const dados = response.data;
+    res.render('index', { dados });
   } catch (error) {
     console.error(error);
-    res.render('index', { dados: [], limit });
+    res.render('index', { dados: [] });
   }
 });
+
+// app.get('/', async (req, res) => {
+//   const limit = parseInt(req.query.limit) || 10; // limite 10 (padrão)
+//   try {
+//     const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json'); 
+//     const dados = response.data.slice(0, limit); 
+//     res.render('index', { dados, limit });
+//   } catch (error) {
+//     console.error(error);
+//     res.render('index', { dados: [], limit });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
